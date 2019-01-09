5c5bfb45dc83846684c0cdec3ed5bf943d0f4b5d
Dario Afonso <alu0100406580@ull.edu.es>
2019-01-09 22:02:19 +0000

    menuDSL ya creado y funcionando pruebas de creación y expectativas para método to_s

--- lib/menu2/menuDSL.rb
+++ lib/menu2/menuDSL.rb
@@ -1,2 +1,40 @@
 class MenuFacil
+    @@desayunos = 1
+    @@almuerzos = 1
+    @@cenas = 1
+    def initialize(name, &block)
+        @nombre = name
+        @desayuno = []
+        @almuerzo = []
+        @cena = []
+    
+        if block_given?  
+          if block.arity == 1
+            yield self
+          else
+           instance_eval(&block) 
+          end
+        end
+      end
+
+    def desayuno(opciones = {})
+        ingrediente = []
+        ingrediente << opciones[:descripcion] << opciones[:porcion] << opciones[:gramos] << Tabla.new(opciones[:grasas],opciones[:grasasSaturadas],opciones[:carbohidratos],opciones[:azucares],opciones[:proteinas],opciones[:sal])
+        @desayuno << ingrediente
+        @@desayunos += 1    
+    end
+    
+    def almuerzo(opciones = {})
+        ingrediente = []
+        ingrediente << opciones[:descripcion] << opciones[:porcion] << opciones[:gramos] << Tabla.new(opciones[:grasas],opciones[:grasasSaturadas],opciones[:carbohidratos],opciones[:azucares],opciones[:proteinas],opciones[:sal])
+        @almuerzo << ingrediente
+        @@almuerzos += 1  
+    end
+
+    def cena(opciones = {})
+        ingrediente = []
+        ingrediente << opciones[:descripcion] << opciones[:porcion] << opciones[:gramos] << Tabla.new(opciones[:grasas],opciones[:grasasSaturadas],opciones[:carbohidratos],opciones[:azucares],opciones[:proteinas],opciones[:sal])
+        @cena << ingrediente
+        @@cenas += 1  
+    end
 end
--- spec/menuDSL_spec.rb
+++ spec/menuDSL_spec.rb
@@ -1,51 +1,61 @@
 RSpec.describe MenuDSL do
 
     before :each do
+        #grasas, grasasSat, hidratos, azucares, proteinas, sal
         @menuFacil = MenuFacil.new("Lunes")do
-            titulo      "Bajo en calorías"
-            ingesta     :min => 30, :max => 35
+            #titulo      "Bajo en calorías"
+            #ingesta     :min => 30, :max => 35
             desayuno    :descripcion => "Pan de trigo integral",
                         :porcion => "1 rodaja",
                         :gramos => 100,
                         :grasas => 3.3,
+                        :grasasSaturadas => 2.0,
                         :carbohidratos => 54.0,
+                        :azucares => 2.3,
                         :proteinas => 11.0,
-                        :fibra => 2.3,
                         :sal => 0.06
             desayuno    :descripcion => "Actimel",
-                        :porcion => "1 porci ́on",
+                        :porcion => "1 porción",
                         :gramos => 100,
                         :grasas =>  3.4,
+                        :grasasSaturadas => 2.4,
                         :carbohidratos => 4.4,
+                        :azucares => 9.8,
                         :proteinas => 3.6,
                         :sal => 0.05
-            almuerzo    :description =>  "Arroz",
+            almuerzo    :descripcion =>  "Arroz",
                         :porcion => "1 taza",
                         :gramos => 100,
                         :grasas => 0.9,
+                        :grasasSaturadas => 0.0,
                         :carbohidratos => 81.6,
                         :proteinas => 6.67,
-                        :fibra => 1.4,
+                        :azucares => 1.4,
                         :sal => 0.04
-            almuerzo    "Lentejas",
+            almuerzo    :descripcion => "Lentejas",
                         :porcion => "1/2 cuchar ́on",
                         :grasas => 0.4,
+                        :grasasSaturadas => 0.0,
                         :carbohidratos => 20.0,
-                        :proteinas => 9.0,
-                        :fibra => 8.0,
+                        :azucares => 8.0,
+                        :proteinas => 9.0,                        
                         :sal => 0.02
             almuerzo    :descripcion => "Naranja",
                         :porcion => "1 pieza",
                         :gramos => 100,
                         :grasas =>  0.12,
+                        :grasasSaturadas => 0.0,
                         :carbohidratos => 11.75,
+                        :azucares => 2.4,
                         :proteinas => 0.94,
-                        :fibra => 2.4
+                        :sal => 0.0
             cena        :descripcion => "Leche entera hacendado",
                         :porcion => "1 vaso",
                         :gramos => 100,
                         :grasas => 3.6,
+                        :grasasSaturadas => 1.1,
                         :carbohidratos => 4.6,
+                        :azucares => 3.6,
                         :proteinas => 3.1,
                         :sal => 0.13
             end
@@ -55,4 +65,9 @@ RSpec.describe MenuDSL do
     it "Creación de un objeto menuDSL" do
         expect(@menuFacil).not_to eq(nil)
       end
+    
+    it "Salida del menuDSL" do
+        expect(@menuFacil.to_s).to be_a_kind_of(String)
+        expect(puts @menuFacil).not_to eq(nil)
+    end        
 end
